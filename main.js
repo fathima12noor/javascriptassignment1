dff322aa8bd79e8e778aea4a37fcf5003926e081
NOOR FATHIMA <93575211+fathima12noor@users.noreply.github.com>
2023-03-15 15:12:02 +0530

    done assignment

---
+++ main.js
@@ -0,0 +1,55 @@
+  var a = 500;
+ console.log(a, typeof a);
+var b = 'noor'
+console.log(b, typeof b);
+
+var c = false
+console.log(c, typeof c);
+
+var d = null
+console.log(d, typeof d);
+ 
+ var e
+ console.log(e, typeof e);
+
+ var f = Symbol();
+ console.log(f, typeof f);
+
+
+var a = 18;
+if (a > 5 )
+{
+  console.log("user is eligible to vote" );
+}
+else
+{
+  console.log("user is not eligible to vote");
+
+}
+
+function Addnumber(c,d)
+{
+  var g = c + d;
+ 
+    return(g);
+}
+console.log(Addnumber(1,5));
+ console.log(Addnumber(2,3));
+
+var student=['ria','sundiya','mega','indira','preethi','fathima'];
+console.log(student);
+
+var array=[10,20,30,50,60];
+console.log(array.length);
+
+  student.push('noor');
+  console.log(student);
+  student.unshift('rao');
+  console.log(student);
+  student.shift('ria');
+  console.log(student);
+  student.pop('mega');
+  console.log(student);
+
+
+
